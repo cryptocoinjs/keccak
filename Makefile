599e09006726fecb6591d97f0807250614abea7d
Kirill Fomichev <fanatid@ya.ru>
2019-12-23 21:07:55 +0300

    Add clang-format

--- Makefile
+++ Makefile
@@ -1,9 +1,23 @@
 .PHONY: lint lint-js
 
+format_cpp_files = src/addon.cc
+
+format: format-cpp
+
+format-cpp:
+	clang-format -i -verbose $(format_cpp_files)
+
+
 standard = ./node_modules/.bin/standard
+lint_dir = build/lint
 
+lint: lint-cpp lint-js
 
-lint: lint-js
+lint-cpp:
+	mkdir -p $(lint_dir)/cpp/src
+	rsync -a --delete src/ $(lint_dir)/cpp/src
+	cd $(lint_dir)/cpp && clang-format -i -verbose $(format_cpp_files)
+	git diff --no-index --exit-code src $(lint_dir)/cpp/src
 
 lint-js:
 	$(standard)
--- src/addon.cc
+++ src/addon.cc
@@ -1,19 +1,19 @@
-#include <node.h>
 #include <nan.h>
+#include <node.h>
 
 extern "C" {
 #if LIBKECCAK == 32
-  #include "libkeccak-32/KeccakSpongeWidth1600.h"
+#include "libkeccak-32/KeccakSpongeWidth1600.h"
 #elif LIBKECCAK == 64
-  #include "libkeccak-64/KeccakSpongeWidth1600.h"
+#include "libkeccak-64/KeccakSpongeWidth1600.h"
 #else
-  #error "LIBKECCAK not defined correctly"
+#error "LIBKECCAK not defined correctly"
 #endif
 }
 
 class KeccakWrapper : public Nan::ObjectWrap {
  public:
-  static v8::Local<v8::Function> Init () {
+  static v8::Local<v8::Function> Init() {
     Nan::EscapableHandleScope scope;
 
     v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
@@ -41,21 +41,29 @@ class KeccakWrapper : public Nan::ObjectWrap {
   static NAN_METHOD(Initialize) {
     KeccakWrapper* obj = Nan::ObjectWrap::Unwrap<KeccakWrapper>(info.Holder());
 #if (NODE_MODULE_VERSION >= NODE_7_0_MODULE_VERSION)
-    unsigned int rate = info[0]->IntegerValue(info.GetIsolate()->GetCurrentContext()).ToChecked();
-    unsigned int capacity = info[1]->IntegerValue(info.GetIsolate()->GetCurrentContext()).ToChecked();
+    unsigned int rate =
+        info[0]
+            ->IntegerValue(info.GetIsolate()->GetCurrentContext())
+            .ToChecked();
+    unsigned int capacity =
+        info[1]
+            ->IntegerValue(info.GetIsolate()->GetCurrentContext())
+            .ToChecked();
 #else
     unsigned int rate = info[0]->IntegerValue();
     unsigned int capacity = info[1]->IntegerValue();
 #endif
 
-    // ignore return code, rate & capacity always will right because internal object
+    // ignore return code, rate & capacity always will right because internal
+    // object
     KeccakWidth1600_SpongeInitialize(&obj->sponge, rate, capacity);
   }
 
   static NAN_METHOD(Absorb) {
     KeccakWrapper* obj = Nan::ObjectWrap::Unwrap<KeccakWrapper>(info.Holder());
     v8::Local<v8::Object> buffer = info[0].As<v8::Object>();
-    const unsigned char* data = (const unsigned char*) node::Buffer::Data(buffer);
+    const unsigned char* data =
+        (const unsigned char*)node::Buffer::Data(buffer);
     size_t length = node::Buffer::Length(buffer);
 
     // ignore return code, bcause internal object
@@ -65,7 +73,10 @@ class KeccakWrapper : public Nan::ObjectWrap {
   static NAN_METHOD(AbsorbLastFewBits) {
     KeccakWrapper* obj = Nan::ObjectWrap::Unwrap<KeccakWrapper>(info.Holder());
 #if (NODE_MODULE_VERSION >= NODE_7_0_MODULE_VERSION)
-    unsigned char bits = info[0]->IntegerValue(info.GetIsolate()->GetCurrentContext()).ToChecked();
+    unsigned char bits =
+        info[0]
+            ->IntegerValue(info.GetIsolate()->GetCurrentContext())
+            .ToChecked();
 #else
     unsigned char bits = info[0]->IntegerValue();
 #endif
@@ -77,13 +88,15 @@ class KeccakWrapper : public Nan::ObjectWrap {
   static NAN_METHOD(Squeeze) {
     KeccakWrapper* obj = Nan::ObjectWrap::Unwrap<KeccakWrapper>(info.Holder());
 #if (NODE_MODULE_VERSION >= NODE_7_0_MODULE_VERSION)
-    size_t length = info[0]->IntegerValue(info.GetIsolate()->GetCurrentContext()).ToChecked();
+    size_t length = info[0]
+                        ->IntegerValue(info.GetIsolate()->GetCurrentContext())
+                        .ToChecked();
 #else
     size_t length = info[0]->IntegerValue();
 #endif
 
     v8::Local<v8::Object> buffer = Nan::NewBuffer(length).ToLocalChecked();
-    unsigned char* data = (unsigned char*) node::Buffer::Data(buffer);
+    unsigned char* data = (unsigned char*)node::Buffer::Data(buffer);
 
     KeccakWidth1600_SpongeSqueeze(&obj->sponge, data, length);
     info.GetReturnValue().Set(buffer);
@@ -92,17 +105,23 @@ class KeccakWrapper : public Nan::ObjectWrap {
   static NAN_METHOD(Copy) {
     KeccakWrapper* from = Nan::ObjectWrap::Unwrap<KeccakWrapper>(info.Holder());
 #if (NODE_MODULE_VERSION >= NODE_7_0_MODULE_VERSION)
-    KeccakWrapper* to = Nan::ObjectWrap::Unwrap<KeccakWrapper>(info[0]->ToObject(info.GetIsolate()->GetCurrentContext()).ToLocalChecked());
+    KeccakWrapper* to = Nan::ObjectWrap::Unwrap<KeccakWrapper>(
+        info[0]
+            ->ToObject(info.GetIsolate()->GetCurrentContext())
+            .ToLocalChecked());
 #else
-    KeccakWrapper* to = Nan::ObjectWrap::Unwrap<KeccakWrapper>(info[0]->ToObject());
+    KeccakWrapper* to =
+        Nan::ObjectWrap::Unwrap<KeccakWrapper>(info[0]->ToObject());
 #endif
 
     memcpy(&to->sponge, &from->sponge, sizeof(KeccakWidth1600_SpongeInstance));
   }
 };
 
-void Init(Nan::ADDON_REGISTER_FUNCTION_ARGS_TYPE exports, Nan::ADDON_REGISTER_FUNCTION_ARGS_TYPE module) {
-  // I wish to use pure functions, but we need wrapper around state (KeccakWidth1600_SpongeInstance)
+void Init(Nan::ADDON_REGISTER_FUNCTION_ARGS_TYPE exports,
+          Nan::ADDON_REGISTER_FUNCTION_ARGS_TYPE module) {
+  // I wish to use pure functions, but we need wrapper around state
+  // (KeccakWidth1600_SpongeInstance)
   Nan::Set(module, Nan::New("exports").ToLocalChecked(), KeccakWrapper::Init());
 }
 
